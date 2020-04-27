2795cf0e78d63a40316cef3fe4f74bd9b2066e86
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-04-27 14:03:40 +0300

    Updated git index

---
+++ src/js/PopupLightBox.js
@@ -0,0 +1,9 @@
+import Popup from "./Popup";
+export default class PopupLightBox extends Popup {
+  constructor(image, popup) {
+    super(popup);
+    this.image = image;
+    popup.querySelector('.popup__picture').src = image.dataset.url;
+    this.toggle(popup);
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+//const FileLoader = require('file-loader');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{ // тут описываются правила
+                test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+                use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+                exclude: /node_modules/ // исключает папку node_modules
+            },
+            {
+                test: /\.css$/, // применять это правило только к CSS-файлам
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'
+                ] // к этим файлам нужно применить пакеты, которые мы уже установили
+            },
+            {
+                test: /\.(png|jpg|gif|ico|svg)$/i,
+                use: [
+                    'file-loader?name=src/images/[name].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [ 
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
+        }),
+        new HtmlWebpackPlugin({ // настроили плагин
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash()
+    ]
+}
